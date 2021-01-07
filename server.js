c0d84549a3e7ca82f111be6c35ff6d9607cde31c
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2021-01-07 10:14:40 -0500

    Update server route

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ import { getAllNovels, getNovelByIdOrTitle } from './controllers/novels'
 
 const app = express()
 
-app.get('/authors', getAllAuthors)
-app.get('/authors/:identifier', getAuthorByIdOrName)
+app.get('/api/authors', getAllAuthors)
+app.get('/api/authors/:identifier', getAuthorByIdOrName)
 
-app.get('/genres', getAllGenres)
-app.get('/genres/:id', getGenreById)
+app.get('/api/genres', getAllGenres)
+app.get('/api/genres/:id', getGenreById)
 
-app.get('/novels', getAllNovels)
-app.get('/novels/:identifier', getNovelByIdOrTitle)
+app.get('/api/novels', getAllNovels)
+app.get('/api/novels/:identifier', getNovelByIdOrTitle)
 
 app.listen(1337, () => {
   console.log('Listening on port 1337...') // eslint-disable-line no-console
