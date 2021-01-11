44f3577ed808562a056575e3fdaa4bf34ac450b8
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2021-01-10 20:27:56 -0500

    Minor changes and remove white space

--- components/Search.jsx
+++ components/Search.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default ({ term, setter }) => (
     <label>
-  <input className="input" placeholder="Search Novel Here" type="text" name="subtitle" value={term} onChange={event => setter(event.target.value)} />
+  <input placeholder="Search Novel Here" type="text" name="search" value={term} onChange={event => setter(event.target.value)} />
   </ label>
 )
