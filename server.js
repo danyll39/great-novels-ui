f9a6d307a3dba13edf6a32217a6ba3360b2472e7
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2021-01-10 20:25:42 -0500

    Fix server  error

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import { getAllAuthors, getAuthorByIdOrName } from './controllers/authors'
 import { getAllGenres, getGenreById } from './controllers/genres'
 import { getAllNovels, getNovelByIdOrTitle } from './controllers/novels'
@@ -15,6 +16,8 @@ app.get('/api/genres/:id', getGenreById)
 
 app.get('/api/novels', getAllNovels)
 app.get('/api/novels/:identifier', getNovelByIdOrTitle)
+
+app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'public', 'index.html')))
 
 app.listen(1337, () => {
   console.log('Listening on port 1337...') // eslint-disable-line no-console
